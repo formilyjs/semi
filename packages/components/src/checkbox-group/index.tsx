c828ae003cef49cfe378566f24413eaa9b40d3c5
prgrmr <programmerwy@163.com>
2025-03-17 23:03:32 +0800

    feat: checkboxgroup 支持复杂 options 结构

--- packages/components/src/checkbox-group/index.tsx
+++ packages/components/src/checkbox-group/index.tsx
@@ -1,10 +1,39 @@
-import { connect, mapProps } from '@formily/react';
-import { CheckboxGroup as SemiCheckboxGroup } from '@douyinfe/semi-ui';
+import React, { FC, useMemo } from "react";
+import { connect, mapProps } from "@formily/react";
+import {
+  Checkbox,
+  CheckboxGroup as SemiCheckboxGroup,
+} from "@douyinfe/semi-ui";
+import { CheckboxGroupProps } from "@douyinfe/semi-ui/lib/es/checkbox";
+
+const OriginCheckboxGroup: FC<CheckboxGroupProps> = (props) => {
+  const { options = [], ...rest } = props;
+
+  const finalOptions = useMemo(() => {
+    return options.map((opt) => {
+      if (typeof opt === "string") {
+        return { label: opt, value: opt };
+      } else {
+        return opt;
+      }
+    });
+  }, [options]);
+
+  return (
+    <SemiCheckboxGroup {...rest}>
+      {finalOptions.map(({ label, value, ...restOpt }, index) => (
+        <Checkbox key={index} value={value} {...restOpt}>
+          {label}
+        </Checkbox>
+      ))}
+    </SemiCheckboxGroup>
+  );
+};
 
 export const CheckboxGroup = connect(
-  SemiCheckboxGroup,
+  OriginCheckboxGroup,
   mapProps({
-    dataSource: 'options',
+    dataSource: "options",
   })
 );
 
